ff64b804480921a43cb0de1cb44c40f4ca720cf9
astmating <131445482+astmating@users.noreply.github.com>
2023-05-11 09:13:53 +0800

    home.tsx

--- app/components/home.tsx
+++ app/components/home.tsx
@@ -108,7 +108,7 @@ function Screen() {
       }
     >
       <SideBar className={isHome ? styles["sidebar-show"] : ""} />
-
+      
       <div className={styles["window-content"]} id={SlotID.AppBody}>
         <Routes>
           <Route path={Path.Home} element={<Chat />} />
