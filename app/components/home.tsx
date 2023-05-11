2406746f57f4a6c866f73f1782da03af20ec0b27
astmating <131445482+astmating@users.noreply.github.com>
2023-05-11 15:17:37 +0800

    home.tsx

--- app/components/home.tsx
+++ app/components/home.tsx
@@ -4,6 +4,9 @@ require("../polyfill");
 
 import { useState, useEffect } from "react";
 
+import React from "react";
+import { UserQycode } from "./UserQycode";
+
 import styles from "./home.module.scss";
 
 import BotIcon from "../icons/bot.svg";
@@ -166,10 +169,9 @@ export function Home() {
   return (
     <ErrorBoundary>
       <Router>
+        <UserQycode /> {/* Add the UserQycode component */}
         <Screen />
-        <UseQycode />
       </Router>
     </ErrorBoundary>
   );
 }
-
